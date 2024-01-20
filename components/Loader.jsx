48d12fe7bdcf7390d15b2d134a812d63d390efd9
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-01-20 14:29:22 +0100

    loader component

---
+++ components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FiLoader } from "react-icons/fi";
+
+const Loader = () => {
+  return (
+    <div className=" w-full mt-12">
+        <FiLoader className='h-32 w-32 mx-auto animate-spin text-muted-foreground' />
+    </div>
+  )
+}
+
+export default Loader
