cd92adc087f810b98c1c8f510f37d12b310b0e84
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-01-20 12:18:07 +0100

    buy button

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Button = ({ product }) => {
+const Button = ({ product, buyProduct }) => {
   return (
     <button  
     type='submit'
-    className='w-fit px-4 py-2 text-xl bg-primary-orange hover:bg-white hover:text-gray-700 border border-primary-orange rounded-md font-satoshi text-white'>
-        {product.cta}
+    onClick={() => buyProduct()}
+    className='w-fit px-4 py-2 text-xl bg-primary-orange hover:bg-white hover:text-gray-700 border border-primary-orange rounded-md font-satoshi text-white'
+    >
+      {product.cta}
     </button>
   )
 }
