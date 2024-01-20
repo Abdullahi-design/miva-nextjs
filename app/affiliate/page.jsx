cd8ea1eeb22d5591f7ff0526c8b83138e5712ba5
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-01-20 09:31:19 +0100

    -[] set affiliate link -[] copy link to clipboard

--- app/affiliate/page.jsx
+++ app/affiliate/page.jsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-
-import Form from "@components/Form";
-import AffiliateForm from "@components/AffiliateForm";
+import { useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
 import ProductCard from "@components/ProductCard";
 
@@ -12,46 +9,69 @@ const AffiliateProduct = () => {
     const searchParams = useSearchParams();
     const productId = searchParams.get("id");
     const { data: session } = useSession();
-    const [product, setProduct] = useState(null);
-    
+    const [products, setProducts] = useState(null);
+    const [affiliateLink, setAffiliateLink] = useState();
 
     useEffect(() => {
         const fetchData = async () => {
-          if (session?.user.id) {
-            // Fetch affiliated products
-            const response = await fetch(`/api/affiliate/generate-affiliate-link/${session.user.id}`);
-            const data = await response.json();
-            // setProduct(data);
-            console.log(data, 'affiliate data');
+            try {
+
+                if (session?.user.id) {
+                    // Fetch affiliated products
+                    const response = await fetch(`/api/affiliate/generate-affiliate-link/${session.user.id}`);
+                    const data = await response.json();
+                    // setProducts(data); // Assuming data is an array of products
+                    console.log(data, 'affiliate data');
     
-            // Check if a product ID exists before making the second API call
-            if (data.productId) {
-              const productResponse = await fetch(`/api/affiliate/${data.productId}`);
-              const productData = await productResponse.json();
-              setProduct(productData);
-              console.log(productData, 'affiliate data product');
+                    // Check if products exist before making the second API call
+                    if (data.length > 0) {
+                        const productPromises = data.map(async (product) => {
+                            setAffiliateLink(product.affiliateLink)
+                            const productResponse = await fetch(`/api/affiliate/${product.productId}`);
+                            return await productResponse.json();
+                        });
+    
+                        // Wait for all promises to resolve before setting products
+                        const productData = await Promise.all(productPromises);
+                        setProducts(productData);
+                        console.log(productData, 'affiliate data products');
+                    }
+                }
+                
+            } catch (error) {
+                console.log(error);
             }
-          }
         };
-    
+
         fetchData();
     }, [session?.user.id]);
 
-    if (!product) {
+    const copyAffiliateLink = () => {
+        if (affiliateLink) {
+          navigator.clipboard.writeText(affiliateLink);
+          // Optionally, you can show a notification or perform any other UI feedback
+          alert("Affiliate link copied to clipboard!");
+        }
+    };
+
+    if (!products) {
         return <div>Loading...</div>; // Add a loading state
     }
 
-    // console.log(product);
-
     return (
-        // <div>hi</div>
-        <ProductCard
-            type='Affiliate'
-            desc='Let people help you sell your product faster.'
-            product={product}
-            setProduct={setProduct}
-            productId={productId}
-        />
+        <div className='mt-10 prompt_layout'>
+            {products.map((product) => (
+                <ProductCard
+                    key={product._id}
+                    type='Affiliate'
+                    desc='Let people help you sell your product faster.'
+                    product={product}
+                    setProduct={setProducts}
+                    productId={productId}
+                    copyAffiliateLink={copyAffiliateLink}
+                />
+            ))}
+        </div>
     );
 };
 
