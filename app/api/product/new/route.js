e38e4883fd8f437628549412d2d1f897344961f1
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-01-28 16:49:53 +0100

    -[] create a product payout page on paystack -[] save the paystack id and slug in the db

--- app/api/product/new/route.js
+++ app/api/product/new/route.js
@@ -2,6 +2,8 @@ import { connectToDB } from "@utils/database";
 import Product from "@models/product";
 
 export const POST = async (request) => {
+    const secretKey = process.env.PAYSTACK_SECRET_KEY;
+
     const { 
         userId, 
         productName, 
@@ -15,23 +17,64 @@ export const POST = async (request) => {
         commission
     } = await request.json();
 
-    try {
-        await connectToDB();
-        const newProduct = new Product({ 
-            creator: userId, 
-            productName, 
-            description,  
-            metaData, 
-            price, 
-            coverImage, 
-            thumbnail, 
-            category, 
-            cta,
-            commission
-        });
+    const options = {
+        hostname: 'api.paystack.co',
+        port: 443,
+        path: '/page',
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${secretKey}`,
+          'Content-Type': 'application/json'
+        }
+    }
 
-        await newProduct.save();
-        return new Response(JSON.stringify(newProduct), { status: 200 })
+    try {
+        
+        const paystackData = {
+            name: productName,
+            description,
+            amount: price * 100, // Convert to kobo (NGN 100 = 10000 kobo)
+        };
+        console.log(paystackData);
+        
+        const paystackResponse = await fetch(`https://${options.hostname}${options.path}`, {
+            ...options,
+            body: JSON.stringify(paystackData),
+        });
+        
+        
+        const paystackResult = await paystackResponse.json();
+        
+        
+        const paystackProductId = paystackResult.data.id;
+        const paystackProductSlug = paystackResult.data.slug;
+        // console.log(paystackResult, 'paystackResult************', paystackProductId); 
+        
+        if (paystackResult.status === true) {
+            await connectToDB();
+            
+            const newProduct = new Product({ 
+                creator: userId,
+                paystackProductId, 
+                paystackProductUrl: `https://paystack.com/pay/${paystackProductSlug}`,
+                productName, 
+                description,  
+                metaData, 
+                price, 
+                coverImage, 
+                thumbnail, 
+                category, 
+                cta,
+                commission
+            });
+        
+            await newProduct.save();
+            return new Response(JSON.stringify(newProduct), { status: 200 })
+        } else {
+            console.error(paystackResult.message);
+            return new Response(JSON.stringify({ error: paystackResult.message }), { status: 500 });
+        }
+
     } catch (error) {
         console.error("Error creating a new product:", error);
 
