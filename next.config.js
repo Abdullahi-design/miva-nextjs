ab646cde5e18d8abde83b27a98ce7710476c9b3e
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-02-03 10:40:51 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-      // appDir: true,
       serverComponentsExternalPackages: ["mongoose"],
     },
     images: {
       domains: ['lh3.googleusercontent.com'],
     },
-    // staticPageGenerationTimeout: 120,
     webpack(config) {
       config.experiments = {
         ...config.experiments,
